26e23473681d2a5f52cedc1c6384d7ebe6d50ca1
Oscar Santamaria Venegas <osantamaria@gmail.com>
2024-09-07 23:14:03 -0600

    backend - adding initial migrations

---
+++ ntd-calculator-service/src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,8 @@
+create table users (
+    id bigint not null auto_increment primary key,
+    username varchar(255) unique not null,
+    password varchar(255) not null,
+    status varchar(255) not null,
+    balance decimal(6,2) not null,
+    created_date timestamp not null
+);
