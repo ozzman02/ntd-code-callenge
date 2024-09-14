738d6b71c302f5563d8c911873ba83d295839000
Oscar Santamaria Venegas <osantamaria@gmail.com>
2024-09-13 19:29:23 -0600

    backend - adding config-server

---
+++ services/ntd-configuration-server/src/test/java/com/ntd/ntd_configuration_server/NtdConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntd.ntd_configuration_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtdConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
